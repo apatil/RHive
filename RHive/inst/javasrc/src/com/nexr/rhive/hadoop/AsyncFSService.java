eb84003b55aa6817159b44c04fbcdb62677da047
alephomega <johan.ahn@nexr.com>
2013-11-18 14:37:50 +0900

    [FEATURE] Parallel copy (from hdfs to local file system)

--- RHive/inst/javasrc/src/com/nexr/rhive/hadoop/AsyncFSService.java
+++ RHive/inst/javasrc/src/com/nexr/rhive/hadoop/AsyncFSService.java
@@ -8,20 +8,21 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 
-public class AsyncFSUtils {
+public class AsyncFSService {
 	private ExecutorService executor;
 	
-	public AsyncFSUtils(int nThreads) {
+	public AsyncFSService(int nThreads) {
 		this.executor = Executors.newFixedThreadPool(nThreads, Executors.defaultThreadFactory());
 	}
 	
-	public void copyToLocal(String id, boolean delSrc, String src, String dst, String defaultFS) throws IOException, InterruptedException, ExecutionException {
+	public void copyToLocal(String id, boolean delSrc, String src, String dst, String defaultFS, boolean cleanIfExists) throws IOException, InterruptedException, ExecutionException {
 		Path srcPath = new Path(src);
 		Path dstPath = new Path(dst);
 
@@ -31,30 +32,36 @@ public class AsyncFSUtils {
 		ExecutorCompletionService<String> completionService = new ExecutorCompletionService<String>(executor);
 		FileStatus fileStatus = fs.getFileStatus(srcPath);
 		
-		int n = 0;
+		int runningTasks = 0;
 		if (fileStatus.isDir()) {
 			FileStatus[] fileStatuses = fs.listStatus(srcPath, new HiddenPathFilter());
+			
 			File dir = new File(String.format("%s%s%s", dst, File.separator, srcPath.getName()));
-			if (!dir.exists()) {
-				dir.mkdirs();
+			if (dir.exists()) {
+				if (cleanIfExists) {
+					FileUtils.cleanDirectory(dir);
+				}
+			} else {
+				FileUtils.forceMkdir(dir);
 			}
 			
 			dstPath = new Path(dir.getAbsolutePath());
-			
-			n = fileStatuses.length;
-			for (int i = 0; i < n; i++) {
+			for (int i = 0; i < fileStatuses.length; i++) {
 				completionService.submit(new CopyToLocalTask(String.format("%s-%d", id, i),
 						delSrc, fileStatuses[i].getPath(), dstPath, defaultFS));
+				
+				runningTasks++;
 			}
+
 		} else {
-			n = 1;
 			completionService.submit(new CopyToLocalTask(id, delSrc, srcPath, dstPath, defaultFS));
+			runningTasks++;
 		}
 		
-		for (int i = 0; i < n; i++) {
+		for (int i = runningTasks; i > 0; i--) {
 			completionService.take();
 		}
-		
+
 		closeFileSystem(fs);
 	}
 	
@@ -87,7 +94,7 @@ public class AsyncFSUtils {
 		}
 	}
 	
-	public void shutdown() {
+	protected void finalize() {
 		executor.shutdown();
 	}
 	
@@ -111,10 +118,9 @@ public class AsyncFSUtils {
 		return conf;
 	}
 	
-	
 	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
-		AsyncFSUtils utils = new AsyncFSUtils(3);
-		utils.copyToLocal("test", false, "/rhive/data", "/home/alephomega", "hdfs://localhost:8020");
-		utils.shutdown();
+		AsyncFSService service = new AsyncFSService(3);
+		service.copyToLocal("copy", false, "/rhive/data", "/home/alephomega", "hdfs://localhost:8020", true);
+		service.executor.shutdown();
 	}
 }
